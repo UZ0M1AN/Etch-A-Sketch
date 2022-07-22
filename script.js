9724b947cc0c4b0c7cd455eff37d90c02d237365
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-07-22 16:45:14 +0100

    Add button to switch between single color and multi-color, Add color picker for single color option, refactor my code

--- script.js
+++ script.js
@@ -1,51 +1,113 @@
 console.log('UZOMIAN!!!');
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////// FUNCTIONS ////////////////////////////////////////////////////////////////////////////
-const changeColor = function(e) {
-    if (e.target == gridDiv) return;
-    e.target.style.backgroundColor = '#eee';
-}
+
+/////////// Event Handlers //////////////////////////
 
 const createGrid = function() {
     if (input.value == '') return;
 
-    // Clear existing grid
+    const val = input.value;
     clearGrid();
 
     // Create new grid
-    for (let i = 0; i < input.value; i++) 
-    for (let j = 0; j < input.value; j++) {
+    for (let i = 0; i < val; i++) 
+    for (let j = 0; j < val; j++) {
         const squareDiv = document.createElement('div');
-        squareDiv.setAttribute('style', `height: ${gridDivSize/input.value}px; width: ${gridDivSize/input.value}px;`)
+        squareDiv.setAttribute('style', `height: ${gridDivSize/val}px; width: ${gridDivSize/val}px;`)
         gridDiv.appendChild(squareDiv);
     }
 
-    console.log(+input.value);
     gridDiv.style.display = 'grid';
-    gridDiv.style.gridTemplateColumns = `repeat(${input.value}, 1fr)`;
+    gridDiv.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
 }
 
 const clearGrid = function() {
+    clearInputField();
+
     gridDiv.innerHTML = '';
     gridDiv.setAttribute('style', `background-color: #272C33; width: ${gridDivSize}px; height: ${gridDivSize}px;`);
 }
 
+const startAndStopDraw = function() {
+    if (gridDiv.innerHTML == '') return;
+    clickCount++;
+    clickCount % 2 ? this.addEventListener('mouseover', changeColor) : this.removeEventListener('mouseover', changeColor);
+}
+
+const changeColor = function(e) {
+    if (e.target == gridDiv) return;
+    e.target.style.backgroundColor = colorBtn.textContent == multiColorText ? currentColor : generateRandomColor();
+}
+
+const changeBtnText = function(e) {
+    e.target.textContent = e.target.textContent == multiColorText ? singleColorText : multiColorText;
+    colorPickerDiv.classList.toggle('hidden');
+}
+
+const changeCurrentColor = function(e) {
+    currentColor = e.target.value;
+}
+
+/////////// Helpers //////////////////////////
+
+const clearInputField = function() {
+    input.value = '';
+}
+
+const generateRandomColor = function() {
+    const h = generateRandomNumber(0, 360);
+    const s = generateRandomNumber(0, 100);
+    const l = generateRandomNumber(40, 90);
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+const generateRandomNumber = function(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Variables
+
+/////////// Variables //////////////////////////
+
+// DOM Variables
 const gridDiv = document.querySelector('.grid');
 const input = document.querySelector('.grid-size--input');
-const inputBtn = document.querySelector('.grid-size--btn');
+const createGridBtn = document.querySelector('.controls--create-btn');
+const clearGridBtn = document.querySelector('.controls--clear-btn');
+const colorBtn = document.querySelector('.color-scheme');
+const colorPickerDiv = document.querySelector('.color-picker');
+const colorPicker = document.querySelector('input[type="color"]');
+
+// Other Variables
 const gridDivSize = 560;
+const multiColorText = 'Use multi-color';
+const singleColorText = 'Use single color';
+let clickCount = 0;
+let currentColor = colorPicker.value;
+
+// Event Listeners
+gridDiv.addEventListener('click', startAndStopDraw);
+createGridBtn.addEventListener('click', createGrid);
+clearGridBtn.addEventListener('click', clearGrid);
+colorBtn.addEventListener('click', changeBtnText);
+colorPicker.addEventListener('change', changeCurrentColor);
+
 
 clearGrid();
 
-// Event Listeners
-gridDiv.addEventListener('mouseover', changeColor);
-inputBtn.addEventListener('click', createGrid);
 
-// 1. Create btn for clearing grid
-// 2. Anytime create grid btn is clicked, clear the input
-// 3. If user clicks while hovering, stop changing color of (drawing on) grid, if user clicks again, resume changing color of grid
-// 4. Extra credit challenge in TOP
-// 5. Cheer your page up a little
+// 1. Create an option to use text input or range input for selecting grid size (dblclick event perhaps?)
+// b. Perhaps add a 'change to multi-color sketch' view
+// 2. Make the color picker hide and display more beautifully
+// 3. Style your multi-color and single-color buttons better with JS (let single-color btn be the color of the currentColor)
+// b. Add a 'pen' heading to describe 3. above
+// 4. Cheer your page up a little
+// 5. Add a dark mode
+
+// Update your instructions
+// Use the radiobtn to select btw range and typed input
+// Click the 'multi-color sketch' btn to see your sketch in multi-color
+// Chose whether you want single-color or multi-color pen
